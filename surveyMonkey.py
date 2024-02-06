cb37739ed755a71e822de55880e1e2872be8e297
Andrej Balabanovic <andrejbalabanovic@yahoo.com>
2024-02-06 20:11:37 +0100

    Used request module for surveymonkey

--- surveyMonkey.py
+++ surveyMonkey.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import requests
 
 parser = argparse.ArgumentParser(
 				prog='Survey monkey 1.0',
@@ -15,18 +16,33 @@ args = parser.parse_args()
 json_file = args.filename
 email_file = args.email
 
-#client = Client('Your_Access_token')
+access_token = 'cOmUpOXfevWx560egh1njmf5pFwFlZFmf5zGe.mlDlE9g43C6F-3hmIBR4M-ciKOwdUTWpwZlQTTbgXGhlABdQxGNBX1hGHxKjJfq9psGXwxzXmilvZC35jeWAJTRXkz'
 
 
 with open(json_file, 'r') as f:
-	data = json.load(f)
+	data_json = json.load(f)
 
-#survey = client.create_survey(data['title'])
+headers = {
 
+	'Authorization': f'Bearer {access_token}',
+	'Content-Type': 'application/json'
+
+}
+
+
+response = requests.post('https://api.surveymonkey.com/v3/surveys', json=data_json, headers=headers)
+
+if response.status_code == 201:
+	survey_id = response.json()['id']
+	survey_link = f'https://www.surveymonkey.com/r/{survey_id}'
+
+else:
+	print(f"Failed to create survey : {response.text}")
+
+"""
 
 for page in data['pages']:
 
-	#page_id = survey.create_page(page['title']).id
 
 	for question in page['questions']:
 
@@ -37,11 +53,8 @@ for page in data['pages']:
 		if question_type == 'multiple_choice':
 
 			print("MC")
-			#survey.create_question(page_id, question_text, 'multiple_choice', choices=choices)
 
-# survey.finalize()
-# survey_link = survey.collectors[0].url
+"""
 
 print("Survey created sucessfully")
-
-
+print(survey_link)
